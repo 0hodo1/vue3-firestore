71f5181810c0d7cb51a098788c7450b744192d19
Ömer Hodo <omerhodo@gmail.com>
2022-03-07 21:45:29 +0300

    firestore initialize generated

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCX0rSU8ag5ivDd7R2Oh3hQJF7A-ACxDFc",
+  authDomain: "vue3-firebase-projects-dc331.firebaseapp.com",
+  projectId: "vue3-firebase-projects-dc331",
+  storageBucket: "vue3-firebase-projects-dc331.appspot.com",
+  messagingSenderId: "483335661311",
+  appId: "1:483335661311:web:ba074ee90f7d6746540eca",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const firestoreRef = firebase.firestore();
+
+export { firestoreRef };
