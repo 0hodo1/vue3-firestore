a3d5e1c330c605cb47f3782d6b90fe781c1c0d64
Ömer Hodo <omerhodo@gmail.com>
2022-03-07 23:06:11 +0300

    firestore update delete set and add operation example added

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCX0rSU8ag5ivDd7R2Oh3hQJF7A-ACxDFc",
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { onMounted } from "@vue/runtime-core";
+import { firestoreRef } from "../firebase/config";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  setup() {
+    onMounted(async () => {
+      // let res = await firestoreRef.collection("personels").get();
+      // res.forEach((doc) => {
+      //   console.log(doc.data());
+      // });
+
+      // let res = await firestoreRef
+      //   .collection("personels")
+      //   .doc("Br5wiFCqFeTT6nlMeQjI")
+      //   .get();
+      // console.log(res.exists);
+
+      // let personelObject = {
+      //   name: "Ahmet",
+      //   role: "user",
+      //   age: 25,
+      // };
+      // let res = await firestoreRef.collection("personels").add(personelObject);
+      // console.log(res);
+
+      // delete Document
+      // await firestoreRef
+      //   .collection("personels")
+      //   .doc("Br5wiFCqFeTT6nlMeQjI")
+      //   .delete();
+
+      // update Document (data update ederken daha önceki verileri varsa günceller yoksa bir şey yapmaz)
+      // await firestoreRef
+      //   .collection("personels")
+      //   .doc("qkniCpLoxzIbgjUKRF62")
+      //   .update({
+      //     name: "omer",
+      //     role: "user",
+      //     city: "İstanbul",
+      //   });
+
+      // set Document (data set ederken daha önceki verileri siler)
+      await firestoreRef
+        .collection("personels")
+        .doc("qkniCpLoxzIbgjUKRF62")
+        .set({
+          city: "İstanbul",
+        });
+    });
+  },
+};
 </script>
